164b5241b1322bbbb6fdc8461c7de50f9da2da3a
Vitor <128196980+VitorGGama@users.noreply.github.com>
2024-02-05 09:50:15 -0300

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,7 +8,7 @@ export default function Document() {
         <meta name="application-name" content="site de esportes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="apple-mobile-web-app-title" content="PetShop" />
+        <meta name="apple-mobile-web-app-title" content="site de esportes" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="theme-color" content="#000000" />
         <link
