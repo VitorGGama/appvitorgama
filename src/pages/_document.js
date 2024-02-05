e4ab7d583d755cd393e0ee7776323609e398bd7a
Vitor <128196980+VitorGGama@users.noreply.github.com>
2024-02-05 09:43:57 -0300

    app

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,8 +2,24 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="pt-br">
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="application-name" content="site de esportes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="PetShop" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#000000" />
+        <link
+          rel="shortcurt icon"
+          href="/images/pistaCorrida.jpg"
+          type="image/jpg"
+          sizes="256x256"
+        />
+        <meta name="author" content="Vitor G. Gama" />
+      </Head>
+
       <body>
         <Main />
         <NextScript />
